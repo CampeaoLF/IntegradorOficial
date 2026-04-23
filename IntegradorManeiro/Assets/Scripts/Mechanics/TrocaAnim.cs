3b982e0be3eb4730cddb3f1c6b32f2786ae85be6
Luis Felipe Tavares Mendes <00124368@unisoacad.uniso.br>
2026-04-22 21:42:54 -0300

    Enum

---
+++ IntegradorManeiro/Assets/Scripts/Mechanics/TrocaAnim.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrocaAnim : MonoBehaviour
+{
+    private string animation;
+    private bool value;
+
+    public TypeOfAnimation animType;
+    public Botton botton;
+
+    public void ChangeAnimation()
+    {
+        
+    }
+}
